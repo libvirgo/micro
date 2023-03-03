00223a8e6ff581c110b3b96fe386c296e627904e
libvirgo <sakurapetgirl@live.com>
2023-03-03 19:27:36 +0800

    auto generate java and go proto.

--- app/item/rpc/src/main/java/libvirgo/github/io/rpc/RpcApplication.java
+++ app/item/rpc/src/main/java/libvirgo/github/io/rpc/RpcApplication.java
@@ -1,13 +1,17 @@
 package libvirgo.github.io.rpc;
 
+import com.google.protobuf.MessageLite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class RpcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RpcApplication.class, args);
+        ArrayList<MessageLite> list = new ArrayList<>();
     }
 
 }
